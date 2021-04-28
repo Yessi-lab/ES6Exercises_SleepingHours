aeda3ca661576bbe030a27be72a505b837492686
Yessi-lab <78738420+Yessi-lab@users.noreply.github.com>
2021-04-28 23:23:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let getSleepHours = day => {
+switch (day) {
+  case 'monday': return 8;
+  break;
+  case 'tuesday': return 8;
+  break;
+  case 'wednesday': return 8;
+  break;
+  case 'thursday': return 8;
+  break;
+  case 'friday': return 8;
+  break;
+  case 'saturday': return 8;
+  break;
+  case 'sunday': return 8;
+  break; 
+}
+};
+
+let getActualSleepHours = () => {
+  
+}
